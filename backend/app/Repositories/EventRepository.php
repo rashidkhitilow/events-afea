59fad3c49f4542ee26cd3c91505235fc76d86fc9
Rashid Khitilov <rashid.khitilov@gmail.com>
2025-05-13 11:37:41 +0400

    I just remembered — the big error is solved now.

--- backend/app/Repositories/EventRepository.php
+++ backend/app/Repositories/EventRepository.php
@@ -2,6 +2,7 @@
 namespace App\Repositories;
 
 use App\Models\Event;
+use Illuminate\Support\Facades\Log;
 
 class EventRepository
 {
@@ -9,28 +10,29 @@ class EventRepository
     {
         // get all event if available seats is greater than 0 and status is not booked and get all available seat numbers
         return Event::where('available_seats', '>', 0)
-    ->with('user_event')
-    ->get()
-    ->map(function ($event) {
-        $takenSeats = $event->user_event->pluck('seat_number')->toArray();
-        $allSeats = range(1, $event->available_seats);
-        $event->available_seat_numbers = array_values(array_diff($allSeats, $takenSeats));
-        return $event;
-    });
+        ->with('user_event')
+        ->get();
     }
 
     public function getById($id)
     {
             return Event::where('available_seats', '>', 0)
-    ->where('id', $id)
-    ->with('user_event')
-    ->get()
-    ->map(function ($event) {
-        $takenSeats = $event->user_event->pluck('seat_number')->toArray();
-        $allSeats = range(1, $event->available_seats);
-        $event->available_seat_numbers = array_values(array_diff($allSeats, $takenSeats));
-        return $event;
-    });
+                ->where('id', $id)
+                ->with('user_event')
+                ->get()
+                ->map(function ($event) {
+                    $takenSeats = $event->user_event->pluck('seat_number')->toArray();
+                    Log::info('takenSeats', ['takenSeats' => $takenSeats]);
+
+                    $allSeats = range(1, $event->total_seats);
+                    Log::info('allSeats', ['allSeats' => $allSeats]);
+
+                    $availableSeats = array_values(array_diff($allSeats, $takenSeats));
+                    $eventArray = $event->toArray();
+                    $eventArray['available_seat_numbers'] = $availableSeats;
+
+                    return $eventArray;
+                });
     }
 
     public function create(array $data)
